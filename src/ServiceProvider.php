81426460de99716965d0ae7290cb56d3b566a4ce
Jonathon Wallen <jonathon@monkii.com>
2017-05-02 10:31:27 +1000

    Basic setup

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jonathon Wallen
+ * @date 21/4/17
+ * @time 10:36 AM
+ * @copyright 2008 - present, Monkii Digital Agency (http://monkii.com.au)
+ */
+
+namespace MonkiiBuilt\LaravelPageSections;
+
+
+class ServiceProvider {
+
+}
